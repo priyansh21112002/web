aad08739ebabcf03ed0b6a4ca8f4da94c05bf1b7
Priyansh Srivastava <82864517+priyansh21112002@users.noreply.github.com>
2023-11-06 11:16:31 +0530

    Add files via upload

---
+++ scrapy.py
@@ -0,0 +1,19 @@
+import scrapy
+
+class MySpider(scrapy.Spider):
+    name = 'myspider'
+    start_urls = ['http://quotes.toscrape.com']
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('span small.author::text').get(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+#scrapy startproject myproject
+#scrapy crawl myspider
